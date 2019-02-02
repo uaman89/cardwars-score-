5cb710837788282785deacb0d725c6de74c312e0
uaman89 <uaman89@gmail.com>
2019-02-03 00:42:50 +0200

    add life count control & 'heal' btn

--- index.js
+++ index.js
@@ -1,28 +1,41 @@
-const fullLife = 25;
+let fullLife = 25;
 let playerLife = fullLife;
 
 const lifeBar = document.querySelector('.health > .health__bar');
 const attackBtns = document.querySelectorAll('.buttons .buttons__attack');
+const fullLifeInput = document.querySelector('#fullLife');
 
-attackBtns;  
-
-function updateLifeBar(){
+function updateLifeBar() {
     const currLifeWidth = lifeBar.clientWidth;
-    lifeBar.style.width = `${playerLife/fullLife * 100}%`;
+    lifeBar.style.width = `${playerLife / fullLife * 100}%`;
 }
 
-function attack(count){
+function attack(count) {
     playerLife -= count;
     updateLifeBar();
-    if (playerLife <= 0 && window.confirm('Game Over')){
-        playerLife = fullLife;
+    if (playerLife <= 0 && window.confirm('Game Over')) {
+        heal();
+    }
+}
+
+function heal() {
+    playerLife = fullLife;
+    updateLifeBar();
+}
+
+function setFullLife() {
+    if (fullLifeInput.value) {
+        fullLife = fullLifeInput.value;
         updateLifeBar();
     }
 }
 
-attackBtns.forEach(btn=>{
+fullLifeInput.value = fullLife;
+
+attackBtns.forEach(btn => {
     btn.onclick = () => {
         attack(parseInt(btn.innerHTML));
     }
 });
+
 
